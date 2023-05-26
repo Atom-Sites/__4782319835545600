250ebf8d6cc32351ad55a221be6b20e061fedc3c
Emma <emma@nicejob.com>
2023-05-26 07:19:43 -0600

    Drop down menu size

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -93,16 +93,16 @@ export default function Example() {
             >
 
             {/*This is the dropdown menu on a larger screen*/}
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-12 w-screen w-60  max-w- overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-trueGray-200">
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-12 w-screen w-64  max-w- overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-trueGray-200">
                 <div className="p-2">
                   {products.map((item) => (
                     <div key={item.name} className="group relative flex items-center gap-x-6 rounded-lg p-3 text-sm leading-6 hover:bg-trueGray-100">
 
-                      {/*Dropdown icons hidden for now*/}  
+                    {/*Dropdown icons hidden for now*/}  
                       <div className="hidden flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-primary-100 group-hover:bg-white">
                         <item.icon className="h-6 w-6 text-gray-600 group-hover:text-primary-600" aria-hidden="true" />
                       </div>
-                      {/*Dropdown Items*/}  
+                    {/*Dropdown Items*/}  
                       <div className="flex-auto">
                         <a href={item.href} className="block font-semibold text-gray-900 ">
                           {item.name}
